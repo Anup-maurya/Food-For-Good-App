d067c35513b88c09456ee6eca70021c4084fed77
Anup Kumar Maurya <62827230+Anup-maurya@users.noreply.github.com>
2024-05-26 00:29:47 +0530

    Update Constant.js

--- Constant.js
+++ Constant.js
@@ -1,5 +1,5 @@
 export const IMAGE_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/";
 
 
 
